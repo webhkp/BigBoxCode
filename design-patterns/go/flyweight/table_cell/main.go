3c9f689e6a197f02cc768fb7c79d4fce8aaf4625
webhkp <webhkp@gmail.com>
2023-06-26 21:39:46 +0600

    Change uumber of rows for the flyweight pattern implementation in go

--- design-patterns/go/flyweight/table_cell/main.go
+++ design-patterns/go/flyweight/table_cell/main.go
@@ -8,7 +8,7 @@ func main() {
 	columnWidths := []int{3, 6, 2, 5, 10}
 	tableCellFactory := NewTableCellFactory()
 
-	for row := 0; row < 100; row++ {
+	for row := 0; row < 1000; row++ {
 		for column := 0; column < len(columnWidths); column++ {
 			tableCell := tableCellFactory.GetTableCell(column, columnWidths[column])
 			tableCell.SetText(fmt.Sprintf("%d-%d", row, column))
