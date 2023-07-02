127d1af7390644ebab08527910a2342574a522a7
webhkp <webhkp@gmail.com>
2023-07-02 07:46:40 +0600

    Remove commented code

--- design-patterns/go/chain_of_responsibility/cache/cache_handler.go
+++ design-patterns/go/chain_of_responsibility/cache/cache_handler.go
@@ -5,16 +5,3 @@ package main
 type CacheHandler interface {
 	HandleRequest(data Data)
 }
-
-// type CacheHandler struct {
-// 	nextCacheHandler *CacheHandler
-// }
-
-// func NewCacheHandler(nextCacheHandler *CacheHandler) (cacheHandler *CacheHandler) {
-// 	cacheHandler = &CacheHandler{}
-// 	cacheHandler.nextCacheHandler = nextCacheHandler
-// 	return
-// }
-
-// func (cacheHandler *CacheHandler) HandleRequest(data *Data) {
-// }
