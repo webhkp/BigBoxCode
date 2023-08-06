9d0acac5aadc7d5f30b91957e27a15d9c409ba8f
webhkp <webhkp@gmail.com>
2023-08-06 09:30:30 +0600

    Cleanup comment

--- design-patterns/ts/visitor/hosting-cost/hosting-calculator-visitor.ts
+++ design-patterns/ts/visitor/hosting-cost/hosting-calculator-visitor.ts
@@ -1,24 +1,8 @@
 // hosting-calculator-visitor.ts
 
-import ComputeService from "./compute-service";
-import ContainerService from "./container-service";
-import DatabaseService from "./database-service";
-import FileStorageService from "./file-storage-service";
-import ServerlessService from "./serverless-service";
 import Service from "./service";
 
 interface HostingCalculatorVisitor {
-
-    // visit(computeService: ComputeService): number;
-
-    // visit(containerService: ContainerService): number;
-
-    // visit(databaseService: DatabaseService): number;
-
-    // visit(fileStorageService: FileStorageService): number;
-
-    // visit(serverlessService: ServerlessService): number;
-
     visit(service: Service): number;
 }
 
