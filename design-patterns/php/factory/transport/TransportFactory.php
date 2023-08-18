a51262385e2d86a74c599b222a3ab023ee085fe1
webhkp <webhkp@gmail.com>
2023-08-18 15:58:58 +0600

    Refactor factory pattern implementation in PHP

--- design-patterns/php/factory/transport/TransportFactory.php
+++ design-patterns/php/factory/transport/TransportFactory.php
@@ -5,23 +5,16 @@ namespace Factory\Transport;
 
 class TransportFactory {
 
-    public static function getTransport($type) {
-        if ($type == NULL) {
-            return NULL;
+    public static function getTransport(string $type): ?Transport {
+        switch (strtolower($type)) {
+            case "bike":
+                return new Bike();
+            case "car":
+                return new Car();
+            case "plane":
+                return new Plane();
         }
 
-        if (strtolower($type) == strtolower("bike")) {
-            return new Bike();
-        }
-
-        if (strtolower($type) == strtolower("car")) {
-            return new Car();
-        }
-
-        if (strtolower($type) == strtolower("plane")) {
-            return new Plane();
-        }
-
-        return NULL;
+        return null;
     }
 }
