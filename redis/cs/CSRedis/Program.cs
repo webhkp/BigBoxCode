5ede41d5dead196bc566a40d5baea3da3ac60055
webhkp <webhkp@gmail.com>
2023-10-07 15:16:46 +0600

    Implement Redis GET command example in C#

---
+++ redis/cs/CSRedis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSRedis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ redis/cs/Get/Program.cs
@@ -0,0 +1,47 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Get
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase rdb = redis.GetDatabase();
+
+
+            /**
+             * Set a key/value
+             *
+             * Command: set firstkey "some value"
+             * Result: OK
+             */
+            var setCommandResult = rdb.StringSet("firstkey", "some value");
+
+            Console.WriteLine("Command: set firstkey \"some value\" | Result: " + setCommandResult);
+
+
+            /**
+             * Check the value of key firstkey
+             *
+             * Command: get firstkey
+             * Result: "some value"
+             */
+            var getCommandResult = rdb.StringGet("firstkey");
+
+            Console.WriteLine("Command: get firstkey | Result: " + getCommandResult);
+
+
+            /**
+             * Check the value of key wrongkey (which does not exist in database)
+             *
+             * Command: get wrongkey
+             * Result: nil
+             */
+            getCommandResult = rdb.StringGet("wrongkey");
+
+            Console.WriteLine("Command: get wrongkey | Result: " + getCommandResult);
+        }
+    }
+}
