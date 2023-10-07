e2670d1fa43d503966b0e2b80be72c98d8612c29
webhkp <webhkp@gmail.com>
2023-10-07 19:57:06 +0600

    Implement Redis GET command example in Ruby

---
+++ redis/ruby/get.rb
@@ -0,0 +1,31 @@
+require 'redis'
+
+redis = Redis.new(host: "localhost", port: 6379)
+
+# redis.set("mykey", "hello world")
+# mykey = redis.get("mykey")
+# print("Command: set firstkey \"some value\" | Result: ",mykey, "\n")
+
+
+# Set a key/value
+# Command: set firstkey "some value"
+# Result: OK
+commandResult = redis.set("firstkey", "some value")
+
+print("Command: set firstkey \"some value\" | Result: ", commandResult, "\n")
+
+
+# Check the value of key firstkey
+# Command: get firstkey
+# Result: "some value"
+commandResult = redis.get("firstkey")
+
+print("Command: get firstkey | Result: ", commandResult, "\n")
+
+
+# Check the value of key wrongkey (which does not exist in database)
+# Command: get wrongkey
+# Result: nil
+commandResult = redis.get("wrongkey")
+
+print("Command: get wrongkey | Result: ", commandResult, "\n")
