bab023bb7e2f1511e9389df4f854aeee2597c603
webhkp <webhkp@gmail.com>
2024-03-08 19:22:34 +0600

    Implement Singly Linked List in typescript(include tests)

---
+++ data-structures/ts/linked-list/singly-linked-list/sll.test.ts
@@ -0,0 +1,477 @@
+// sll.test.js
+import { describe, it, beforeEach, expect } from 'vitest';
+import SinglyLinkedList from "./sll";
+
+describe("SinglyLinkedList", () => {
+  describe("Push item to SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+    });
+
+    it("Should have correct head and tail", () => {
+      // Initially head and tails should be null
+      expect(singlyLinkedList.head).toBeNull();
+      expect(singlyLinkedList.tail).toBeNull();
+
+      singlyLinkedList.push("Big");
+
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Big");
+
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+    });
+
+    it("Should have correct length", () => {
+      expect(singlyLinkedList.length).toBe(0);
+
+      singlyLinkedList.push("Big");
+
+      expect(singlyLinkedList.length).toBe(1);
+
+      singlyLinkedList.push("Box");
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      singlyLinkedList.push("Code");
+
+      expect(singlyLinkedList.length).toBe(3);
+    });
+  });
+
+  describe("Pop item from SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should pop item properly", () => {
+      const pop1 = singlyLinkedList.pop();
+
+      expect(pop1!.data).toBe("Code");
+
+      const pop2 = singlyLinkedList.pop();
+
+      expect(pop2!.data).toBe("Box");
+
+      const pop3 = singlyLinkedList.pop();
+
+      expect(pop3!.data).toBe("Big");
+
+      const pop0 = singlyLinkedList.pop();
+
+      expect(pop0).toBeNull();
+    });
+
+    it("Should have correct head and tail", () => {
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+
+      singlyLinkedList.pop();
+
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Box");
+
+      singlyLinkedList.pop();
+
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Big");
+
+      singlyLinkedList.pop();
+
+      expect(singlyLinkedList.head).toBeNull();
+      expect(singlyLinkedList.tail).toBeNull();
+    });
+
+    it("Should have correct length", () => {
+      const pop1 = singlyLinkedList.pop();
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      const pop2 = singlyLinkedList.pop();
+
+      expect(singlyLinkedList.length).toBe(1);
+
+      const pop3 = singlyLinkedList.pop();
+
+      expect(singlyLinkedList.length).toBe(0);
+
+      const pop0 = singlyLinkedList.pop();
+
+      expect(singlyLinkedList.length).toBe(0);
+    });
+  });
+
+  describe("Shift item from SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should shift head properly", () => {
+      const shift1 = singlyLinkedList.shift();
+
+      expect(shift1!.data).toBe("Big");
+
+      const shift2 = singlyLinkedList.shift();
+
+      expect(shift2!.data).toBe("Box");
+
+      const shift3 = singlyLinkedList.pop();
+
+      expect(shift3!.data).toBe("Code");
+
+      const shift0 = singlyLinkedList.pop();
+
+      expect(shift0).toBeNull();
+    });
+
+    it("Should have correct head and tail", () => {
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.head!.data).toBe("Box");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.head!.data).toBe("Code");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.head).toBeNull();
+      expect(singlyLinkedList.tail).toBeNull();
+    });
+
+    it("Should have correct length", () => {
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.length).toBe(1);
+
+      singlyLinkedList.shift();
+
+      expect(singlyLinkedList.length).toBe(0);
+
+      // Try to pop one more time
+      singlyLinkedList.pop();
+
+      expect(singlyLinkedList.length).toBe(0);
+    });
+  });
+
+  describe("Unshift item from SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+    });
+
+    it("Should unshift head properly", () => {
+      const unshift1 = singlyLinkedList.unshift("Code");
+
+      expect(unshift1).not.toBeNull();
+
+      const unshift2 = singlyLinkedList.unshift("Box");
+
+      expect(unshift2).not.toBeNull();
+    });
+
+    it("Should have correct head and tail", () => {
+      singlyLinkedList.unshift("Code");
+
+      expect(singlyLinkedList.head!.data).toBe("Code");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+
+      singlyLinkedList.unshift("Box");
+
+      expect(singlyLinkedList.head!.data).toBe("Box");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+
+      singlyLinkedList.unshift("Big");
+
+      expect(singlyLinkedList.head!.data).toBe("Big");
+      expect(singlyLinkedList.tail!.data).toBe("Code");
+    });
+
+    it("Should have correct length", () => {
+      singlyLinkedList.unshift("Code");
+
+      expect(singlyLinkedList.length).toBe(1);
+
+      singlyLinkedList.unshift("Box");
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      singlyLinkedList.unshift("Big");
+
+      expect(singlyLinkedList.length).toBe(3);
+    });
+  });
+
+  describe("Get item from SinglyLinkedList by index", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should return node at specific index", () => {
+      const node0 = singlyLinkedList.get(0);
+
+      expect(node0!.data).toBe("Big");
+
+      const node2 = singlyLinkedList.get(2);
+
+      expect(node2!.data).toBe("Code");
+    });
+
+    it("Should return null for invalid index", () => {
+      const nodenve = singlyLinkedList.get(-1);
+
+      expect(nodenve).toBeNull();
+
+      const nodelarge = singlyLinkedList.get(999999);
+
+      expect(nodelarge).toBeNull();
+    });
+  });
+
+  describe("Search item in SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should return correct index of the item", () => {
+      const search0 = singlyLinkedList.search("Big");
+
+      expect(search0).toBe(0);
+
+      const search2 = singlyLinkedList.search("Code");
+
+      expect(search2).toBe(2);
+    });
+
+    it("Should return null for non existing item", () => {
+      const searchResult = singlyLinkedList.search("Non existing item");
+
+      expect(searchResult).toBeNull();
+    });
+  });
+
+  describe("Set item to SinglyLinkedList by index", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should set value at node properly", () => {
+      const set0 = singlyLinkedList.set(0, "New Val");
+
+      expect(set0).toBeTruthy();
+
+      const get0 = singlyLinkedList.get(0);
+
+      expect(get0!.data).toBe("New Val");
+
+      const set2 = singlyLinkedList.set(2, "Second");
+
+      expect(set2).toBeTruthy();
+
+      const get2 = singlyLinkedList.get(2);
+
+      expect(get2!.data).toBe("Second");
+
+      const setN = singlyLinkedList.set(99, "Out Bound");
+
+      expect(setN).toBeFalsy();
+    });
+
+    it("Should handle empty list without any exception", () => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+
+      const set0 = singlyLinkedList.set(0, "Out Bound");
+
+      expect(set0).toBeFalsy();
+    });
+  });
+
+  describe("Insert item to SinglyLinkedList by index", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+    });
+
+    it("Should insert data at index", () => {
+      const insert0 = singlyLinkedList.insert(0, "Big");
+
+      expect(insert0).toBeTruthy();
+
+      const get0 = singlyLinkedList.get(0);
+
+      expect(get0!.data).toBe("Big");
+
+      const insert1 = singlyLinkedList.insert(1, "Code");
+
+      expect(insert1).toBeTruthy();
+
+      const get1 = singlyLinkedList.get(1);
+
+      expect(get1!.data).toBe("Code");
+
+      const insert1_1 = singlyLinkedList.insert(1, "Box");
+
+      expect(insert1_1).toBeTruthy();
+
+      const get1_1 = singlyLinkedList.get(1);
+
+      expect(get1_1!.data).toBe("Box");
+
+      const get1_2 = singlyLinkedList.get(2);
+
+      expect(get1_2!.data).toBe("Code");
+
+      const setN = singlyLinkedList.insert(99, "Out Bound");
+
+      expect(setN).toBeFalsy();
+    });
+
+    it("Should have proper length after insert", () => {
+      singlyLinkedList.insert(0, "Big");
+
+      expect(singlyLinkedList.length).toBe(1);
+
+      singlyLinkedList.insert(1, "Code");
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      singlyLinkedList.insert(1, "Box");
+
+      expect(singlyLinkedList.length).toBe(3);
+
+      singlyLinkedList.insert(99, "Out Bound");
+
+      expect(singlyLinkedList.length).toBe(3);
+    });
+  });
+
+  describe("Remove item from SinglyLinkedList by index", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+    });
+
+    it("Should remove data at index", () => {
+      const remove2 = singlyLinkedList.remove(2);
+
+      expect(remove2!.data).toBe("Code");
+
+      const get2 = singlyLinkedList.get(2);
+
+      expect(get2).toBeNull();
+
+      const remove0 = singlyLinkedList.remove(0);
+
+      expect(remove0!.data).toBe("Big");
+
+      const get0 = singlyLinkedList.get(0);
+
+      expect(get0!.data).toBe("Box");
+
+      const removeN = singlyLinkedList.remove(99);
+
+      expect(removeN).toBeFalsy();
+    });
+
+    it("Should have proper length after remove", () => {
+      singlyLinkedList.remove(1);
+
+      expect(singlyLinkedList.length).toBe(2);
+
+      singlyLinkedList.remove(99);
+
+      expect(singlyLinkedList.length).toBe(2);
+    });
+  });
+
+  describe("Reverse SinglyLinkedList", () => {
+    let singlyLinkedList: SinglyLinkedList<string>;
+
+    beforeEach(() => {
+      singlyLinkedList = new SinglyLinkedList<string>();
+      singlyLinkedList.push("Big");
+      singlyLinkedList.push("Box");
+      singlyLinkedList.push("Code");
+      singlyLinkedList.push("Singly");
+      singlyLinkedList.push("Linked");
+      singlyLinkedList.push("List");
+    });
+
+    it("Should set head properly", () => {
+      singlyLinkedList.reverse();
+
+      expect(singlyLinkedList.head!.data).toBe("List");
+    });
+
+    it("Should set tail properly", () => {
+      singlyLinkedList.reverse();
+
+      expect(singlyLinkedList.tail!.data).toBe("Big");
+    });
+
+    it("Should set all elements properly", () => {
+      singlyLinkedList.reverse();
+
+      const get1 = singlyLinkedList.get(1);
+
+      expect(get1!.data).toBe("Linked");
+
+      const get2 = singlyLinkedList.get(2);
+
+      expect(get2!.data).toBe("Singly");
+
+      const get4 = singlyLinkedList.get(4);
+
+      expect(get4!.data).toBe("Box");
+    });
+
+    it("Should not change the length", () => {
+      singlyLinkedList.reverse();
+
+      expect(singlyLinkedList.length).toBe(6);
+    });
+  });
+});
---
+++ data-structures/ts/linked-list/singly-linked-list/sll.ts
@@ -0,0 +1,258 @@
+// sll.js
+// Singly linked list in JavaScript
+
+class Node<T> {
+  // Node class for storing data and reference to the next node
+  data: T;
+  next: Node<T> | null;
+
+  constructor(data: T) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList<T> {
+  // Singly Linked List class
+  public head: Node<T> | null;
+  public tail: Node<T> | null;
+  public length: number;
+
+  constructor() {
+    // Initialize head, tail, and length
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data: T): SinglyLinkedList<T> {
+    // Add data at the end of the list
+    const node = new Node(data);
+
+    if (!this.head) {
+      // If list is empty, set head and tail to new node
+      this.head = node;
+    } else {
+      // Otherwise, set the next of current tail to new node
+      this.tail!.next = node;
+    }
+
+    // Set new node as the tail
+    this.tail = node;
+    // Increase the length
+    this.length++;
+    return this;
+  }
+
+  pop(): Node<T> | null {
+    // Remove and return the last element from the list
+    if (!this.head) {
+      // If list is empty, return null
+      return null;
+    }
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      // Traverse the list to find the second last node
+      newTail = current;
+      current = current.next;
+    }
+
+    // Update tail and remove the last node
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      // If list becomes empty, reset head and tail
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+
+  traverse(): void {
+    // Traverse the list and log the data of each node
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  shift(): Node<T> | null {
+    // Remove and return the first element from the list
+    if (!this.head) {
+      // If list is empty, return null
+      return null;
+    }
+
+    const currHead = this.head;
+    // Move head to the next node
+    this.head = currHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      // If list becomes empty, reset head and tail
+      this.head = null;
+      this.tail = null;
+    }
+
+    return currHead;
+  }
+
+  unshift(value: T): SinglyLinkedList<T> {
+    // Add data at the beginning of the list
+    let newHead = new Node(value);
+
+    if (!this.head) {
+      // If list is empty, set head and tail to new node
+      this.tail = newHead;
+    } else {
+      // Otherwise, set the next of new node to current head
+      newHead.next = this.head;
+    }
+
+    // Set new node as the head
+    this.head = newHead;
+    // Increase the length
+    this.length++;
+    return this;
+  }
+
+  get(index: number): Node<T> | null {
+    // Get node at a specified index
+    if (index < 0 || index >= this.length) {
+      // If index is out of range, return null
+      return null;
+    }
+
+    let selectedNode = this.head;
+
+    for (let i = 1; i <= index; i++) {
+      // Traverse the list to find the node at the given index
+      selectedNode = selectedNode!.next;
+    }
+
+    return selectedNode;
+  }
+
+  search(data: T): number | null {
+    // Search for the index of a specific data in the list
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      // Traverse the list and check data of each node
+      if (current.data === data) {
+        // If data is found, return its index
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    // If data is not found, return null
+    return null;
+  }
+
+  set(index: number, data: T): boolean {
+    // Set data of a specific node at a given index
+    let nodeAtIndex = this.get(index);
+
+    if (nodeAtIndex) {
+      // If node at given index exists, update its data
+      nodeAtIndex.data = data;
+      return true;
+    }
+
+    // If node at given index does not exist, return false
+    return false;
+  }
+
+  insert(index: number, data: T): boolean {
+    // Insert new node at a specific index
+    if (index < 0 || index > this.length) {
+      // If index is out of range, return false
+      return false;
+    }
+
+    if (index === this.length) {
+      // If index is at the end, use push method
+      this.push(data);
+      return true;
+    }
+
+    if (index === 0) {
+      // If index is at the beginning, use unshift method
+      this.unshift(data);
+      return true;
+    }
+
+    // Otherwise, insert new node at the specified index
+    let newNode = new Node(data);
+    let prevNode = this.get(index - 1);
+    newNode.next = prevNode!.next;
+    prevNode!.next = newNode;
+
+    // Increase length
+    this.length++;
+    return true;
+  }
+
+  remove(index: number): Node<T> | null {
+    // Remove node at a specific index
+    if (index < 0 || index >= this.length) {
+      // If index is out of range, return undefined
+      return null;
+    }
+
+    if (index === 0) {
+      // If index is at the beginning, use shift method
+      return this.shift();
+    }
+
+    if (index === this.length - 1) {
+      // If index is at the end, use pop method
+      return this.pop();
+    }
+
+    // Otherwise, remove node at the specified index
+    let prevNode = this.get(index - 1);
+    const removedNode = prevNode!.next;
+    prevNode!.next = prevNode!.next!.next;
+
+    // Decrease length
+    this.length--;
+    return removedNode;
+  }
+
+  reverse(): SinglyLinkedList<T> {
+    // Reverse the linked list
+    let currentNode = this.head;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currentNode) {
+      // Traverse the list and reverse each node
+      nextNode = currentNode.next;
+      currentNode.next = prevNode;
+
+      prevNode = currentNode;
+      currentNode = nextNode;
+    }
+
+    // Update head and tail references
+    this.tail = this.head;
+    this.head = prevNode;
+
+    return this;
+  }
+}
+
+export default SinglyLinkedList;
